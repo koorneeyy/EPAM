773a848872f6453df7a8ac5647ac98f37a63f689
Serhii Korniichuk <koorneeyy@gmail.com>
2018-10-19 14:11:20 +0300

    Task 3_1

---
+++ src/task3_1/controller/ToyController.java
@@ -0,0 +1,20 @@
+package task3_1.controller;
+
+
+import task3_1.data.DataSource;
+import task3_1.model.ToyRoom;
+import task3_1.view.ToysView;
+
+public class ToyController {
+    ToyRoom model = new ToyRoom();
+    ToysView view = new ToysView();
+
+    public void run() {
+        model.setToys(DataSource.getToys());
+        view.printToys(ToysView.ALL_TOYS, model.getToys());
+        view.printMessage(ToysView.PRICE_SUM + String.valueOf(model.getTotalPrice()));
+        view.printMessage(ToysView.COUNT + String.valueOf(model.getToysCountInRoom()));
+        view.printToys(ToysView.BY_NAME, model.orderByName());
+        view.printToys(ToysView.BY_AGE_FILTER, model.getToysByAge(5));
+    }
+}
